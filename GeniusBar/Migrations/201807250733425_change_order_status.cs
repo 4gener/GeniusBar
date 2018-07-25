0f64510cb44ce646f8a6bc5af79289020dfc962a
suniique <32534476+suniique@users.noreply.github.com>
2018-07-25 17:02:00 +0800

    add migrations files

---
+++ GeniusBar/Migrations/201807250733425_change_order_status.cs
@@ -0,0 +1,18 @@
+﻿namespace GeniusBar.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class change_order_status : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("GENIUSBAR.RepairOrders", "State", c => c.Decimal(nullable: false, precision: 10, scale: 0));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("GENIUSBAR.RepairOrders", "State", c => c.Decimal(nullable: false, precision: 3, scale: 0));
+        }
+    }
+}
